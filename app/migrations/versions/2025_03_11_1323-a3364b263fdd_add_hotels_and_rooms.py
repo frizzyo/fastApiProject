3dc4bd55bbe9b17e08136a1ecf76d6792df4415a
frizzyo <hsobol04@mail.ru>
2025-03-11 13:36:59 +0300

    install black update migration file name

---
+++ app/migrations/versions/2025_03_11_1323-a3364b263fdd_add_hotels_and_rooms.py
@@ -0,0 +1,50 @@
+"""add hotels and rooms
+
+Revision ID: a3364b263fdd
+Revises:
+Create Date: 2025-03-11 13:23:26.105300
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "a3364b263fdd"
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "hotels",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(length=100), nullable=False),
+        sa.Column("location", sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint("id"),
+    )
+    op.create_table(
+        "rooms",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("hotel_id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("description", sa.String(), nullable=True),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.Column("quantity", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["hotel_id"],
+            ["hotels.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("rooms")
+    op.drop_table("hotels")
