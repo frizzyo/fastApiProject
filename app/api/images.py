f6960e2330b04f12e5b000ca5747314fb7429f7a
frizzyo <hsobol04@mail.ru>
2025-05-13 14:58:34 +0300

    background task

--- app/api/images.py
+++ app/api/images.py
@@ -1,7 +1,7 @@
 import shutil
 from pathlib import Path
 
-from fastapi import APIRouter, UploadFile
+from fastapi import APIRouter, UploadFile, BackgroundTasks
 
 from app.tasks.tasks import resize_image
 
@@ -9,8 +9,10 @@ router = APIRouter(prefix="/images", tags=["Изображения отелей"
 
 
 @router.post("")
-def upload_image(file: UploadFile):
-    with open(f"{Path(__file__).parent.parent/'static/images'}/{file.filename}", "wb+") as new_file:
+def upload_image(file: UploadFile, background_tasks: BackgroundTasks):
+    path = f"{Path(__file__).parent.parent/'static/images'}/"
+    with open(f"{path}{file.filename}", "wb+") as new_file:
         shutil.copyfileobj(file.file, new_file)
 
-    resize_image.delay(f"{Path(__file__).parent.parent/'static/images'}/{file.filename}")
+    resize_image.delay(f"{path}{file.filename}")
+    # background_tasks.add_task(resize_image, f"{path}{file.filename}")
