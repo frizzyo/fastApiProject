2c233616f1ab37015b242494beb916815bf16cd3
frizzyo <hsobol04@mail.ru>
2025-05-14 15:54:47 +0300

    fixture for hotel and rooms

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,12 @@ import pytest
 from httpx import AsyncClient, ASGITransport
 
 from app.config import settings
-from app.database import Base, engine_null_pool
+from app.database import Base, engine_null_pool, async_session_maker_null_pool
 from app.main import app
 from app.models import *
+from app.schemas.hotels import HotelAdd
+from app.schemas.rooms import RoomAdd
+from app.utils.db_manager import DBManager
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -17,6 +20,19 @@ async def create_database():
     async with engine_null_pool.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+    with open("tests/mock_hotels.json", encoding="utf-8") as file:
+        hotels_data = json.load(file)
+    hotels = [HotelAdd.model_validate(hotel) for hotel in hotels_data]
+
+    with open("tests/mock_rooms.json", encoding="utf-8") as file:
+        rooms_data = json.load(file)
+    rooms = [RoomAdd.model_validate(room) for room in rooms_data]
+
+    async with DBManager(session_factory=async_session_maker_null_pool) as db:
+        await db.hotels.add_bulk(hotels)
+        await db.rooms.add_bulk(rooms)
+        await db.commit()
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -28,26 +44,3 @@ async def fill_database(create_database):
             "email": "kot@pec.com",
             "password": "kotopec"
         })
-        with open(f"{Path(__file__).parent/'mock_hotels.json'}", encoding="utf-8") as file:
-            hotels_data = json.load(file)
-            for hotel in hotels_data:
-                await ac.post(
-                    "/hotels/",
-                    json={
-                        "title": hotel["title"],
-                        "location": hotel["location"],
-                    }
-                )
-
-        with open(f"{Path(__file__).parent/'mock_rooms.json'}", encoding="utf-8") as file:
-            rooms_data = json.load(file)
-            for room in rooms_data:
-                await ac.post(
-                    f"/hotels/{room['hotel_id']}/rooms/",
-                    json={
-                        "title": room["title"],
-                        "description": room["description"],
-                        "price": room["price"],
-                        "quantity": room["quantity"],
-                    }
-                )
